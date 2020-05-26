7d3202ed5de586f35f267556ee68df9f417c382d
Shamseena <52490745+shamseena-dev@users.noreply.github.com>
2020-05-26 18:35:11 +0200

    Update About.js

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -9,22 +9,19 @@ function About() {
     <div className="container">
       <h4 className="center">About Me </h4>
       <img src={aboutBanner} className="about-banner" alt="About_banner pic" />
-      <p className="center">
-        I am SHAMSEENA KARUMAROT (Shamz), A passionate Web/Front-End Developer
-        currently residing in Berlin,Germany.
-      </p>
-
-      <p>
-        I am a B.Tech Computer Science & Engineering graduate with 'First class
-        with Honors'.I have 2+ years of experience as Systems Engineer at Tata
-        Consultancy Services, where I handled Level2 Tech Support of SAP
-        Enterprise Portal ,EMEA. I have work experience in UX/UI Manual Testing
-        and WordPress Content Management as well. 
-      </p>
-
-      <p>
-        My love and passion for Web Development made me dive into
-        this amazing field .
+      <p className="center about-notes">
+         I am Shamseena Karumarot (Shamz), a highly passionate Web/Front-End/software Developer. I love building aesthetically pleasing and amazing websites.
+       </p>
+   <p>
+    Talking a bit about my background, I graduated in Computer Science & Engineering (B.Tech) and grabbed my first job offer in hand while still in 6th semester. It was a dream come true as I joined Tata Consultancy Services within 2 weeks of completion my college days.  
+        I worked for 2+ years as Systems Engineer at T.C.S(Bangalore) where I handled Level 2 tech support for SAP ERP, EMEA region.  After this, I got a couple of project experience in UX/UI Manual Testing
+        for a start up and WordPress Content Management for an N.G.O .
+        </p>
+  <p>
+        I have always been passionate to dive into the Development field and loved to get my hands dirty with code. I have been learning Web development from various sources for while and recently got graduated in Web Development from ZTM academy . I have built a couple of personal projects .I could also work for corona-help.org as Frontend Developer.
+I love taking part in hackathons ,love to mentor and to assist social ventures and N.G.Os.         
+</p>
+     
       </p>
       </div>
       </div>
