e7ad18a400fe830d8ffb81c0642c0c29ad3a7347
Shamseena <52490745+shamseena-dev@users.noreply.github.com>
2020-05-26 19:08:17 +0200

    Update About.js

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -14,12 +14,12 @@ function About() {
        </p>
    <p>
     Talking a bit about my background, I graduated in Computer Science & Engineering (B.Tech) and grabbed my first job offer in hand while still in 6th semester. It was a dream come true as I joined Tata Consultancy Services within 2 weeks of completion my college days.  
-        I worked for 2+ years as Systems Engineer at T.C.S(Bangalore) where I handled Level 2 tech support for SAP ERP, EMEA region.  After this, I got a couple of project experience in UX/UI Manual Testing
+        I worked for 2+ years as Systems Engineer at T.C.S(Bangalore) where I handled Level 2 tech support for SAP ERP, EMEA region.  After this, I earned a couple of project experience in UX/UI Manual Testing
         for a start up and WordPress Content Management for an N.G.O .
         </p>
   <p>
         I have always been passionate to dive into the Development field. I have been learning Web development from various sources for a while and recently got graduated in Web Development from ZTM academy . I have built a couple of personal projects .I could also work for corona-help.org as Frontend Developer.
-I love taking part in hackathons ,love to mentor and to assist social ventures and N.G.Os.         
+I love to take part in hackathons ,to mentor and assist social ventures and N.G.O's in my extra time.         
 </p>
      
      
