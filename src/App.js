1fb1406693452bd273017b98d6461bc2a0d01dc5
Shamseena <52490745+shamseena-dev@users.noreply.github.com>
2020-08-04 22:45:01 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import logo from './1.svg';
-import Scrollspy from 'react-scrollspy'
 import './App.css';
 import Navbar from "./Components/Navbar/Navbar";
 import About from "./Components/About/About";
