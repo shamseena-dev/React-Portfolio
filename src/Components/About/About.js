90e37a8b9c000204eede2fb79af87000be363588
Shamseena <52490745+shamseena-dev@users.noreply.github.com>
2020-08-04 12:03:33 +0200

    Update About.js

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -10,15 +10,19 @@ function About() {
       <h4 className="center">About Me </h4>
       <img src={aboutBanner} className="about-banner" alt="About_banner pic" />
       <p className="center about-notes">
-         I am Shamseena Karumarot (Shamz), a highly passionate Web/Front-End/software Developer. I love building aesthetically pleasing and amazing User experiences.
+         I am Shamseena Karumarot (Shamz), a passionate Web/Front-End/software Developer. I love building aesthetically pleasing and amazing User experiences.
        </p>
+  
+  
+        <p>Currently I am working as the Front End Developer and Lead Engineer at PurpleConnect , an AI powered startup in Healthcare . </P>
    <p>
-    Talking a bit about my background, I graduated in Computer Science & Engineering (B.Tech) and grabbed my first job offer in hand while still in 6th semester. It was a dream come true as I joined Tata Consultancy Services within 2 weeks of completion my college days.  
-        I worked for 2+ years as Systems Engineer at T.C.S(Bangalore) where I handled Level-2 tech support for SAP ERP, EMEA region.  After this, I earned a couple of project experience in UX/UI Manual Testing
+    Talking a bit about my background, I graduated in Computer Science & Engineering (B.Tech) and grabbed my first job offer through campus placement in one of the reputed MNCs  -Tata Consultancy Services.  
+        I worked for 2+ years as Systems Engineer at T.C.S(Bangalore) where I handled Level-2 tech support for SAP ERP, EMEA region.  After this, I acquired a couple of project experience in UX/UI Manual Testing
         for a start up and WordPress Content Management for an N.G.O .
         </p>
-  <p>
-        I have always been passionate to dive into the Development field. I have been learning Web development from multiple sources for a while and recently got graduated in Web Development from ZTM academy . I have built a couple of personal projects and have volunteered for corona-help.org as Frontend Developer.
+ 
+        
+       <p> I have also built a couple of personal projects and have volunteered for corona-help.org as Frontend Developer.
 I love to take part in hackathons ,to mentor and assist social ventures and N.G.O's in my extra time.         
 </p>
      
